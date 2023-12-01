4af6bc2c220fdf09b5215814e6b62ed22beb8a16
Daan Troost <d.q.troost@outlook.com>
2023-12-01 11:45:01 +0100

    init commit

---
+++ src/main/java/nl/miwnn/c12/dqtroost/yeOldeGunShoppeAPI/YeOldeGunShoppeApiApplication.java
@@ -0,0 +1,13 @@
+package nl.miwnn.c12.dqtroost.yeOldeGunShoppeAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeOldeGunShoppeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YeOldeGunShoppeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/miwnn/c12/dqtroost/yeOldeGunShoppeAPI/YeOldeGunShoppeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.miwnn.c12.dqtroost.yeOldeGunShoppeAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeOldeGunShoppeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
